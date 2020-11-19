6ccb30bd6f59e12329c1cb488f949607afaa38d7
磯部浩樹 <h.sep23@gmail.com>
2020-11-19 11:45:00 +0900

    [Add]rgb to_ints

--- cherry-book/4章/rgb.rb
+++ cherry-book/4章/rgb.rb
@@ -1,5 +1,17 @@
+#10進数の数値を16進数に変換した文字列を返すメソッド
 def to_hex(r, g, b)
   [r, g, b].inject('#') do |hex, n|
     hex + n.to_s(16).rjust(2, '0')
   end
 end
+
+#16進数の文字列を10進数の数値3つに変換するメソッド
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  [r, g, b].map do |s|
+    s.hex
+  end
+end
+
