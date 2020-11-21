e341d53e5647b596a9df6b158010c799c156310a
磯部浩樹 <h.sep23@gmail.com>
2020-11-21 11:36:50 +0900

    [Update]chapter5-1

--- cherry-book/5章/chapter5-1.rb
+++ cherry-book/5章/chapter5-1.rb
@@ -8,3 +8,7 @@ currencies.keys #=> [:japan, :us, :india]
 currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
 currencies.values #=> ["yen", "dollar", "rupee"]
 
+#has_key?/key?/include?/member?(has_key?はハッシュの中に指定したキーが存在するか確認メソッド。他はエイリアスメソッド。)
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.has_key?(:japan) #=> true
+currencies.has_key?(:italy) #=> false
