04383ab422776220e6f04ee09d415953be436f00
磯部浩樹 <h.sep23@gmail.com>
2020-11-21 11:32:46 +0900

    [Update]chapter5-1

---
+++ cherry-book/5章/chapter5-1.rb
@@ -0,0 +1,10 @@
+#ハッシュ色々
+
+#keys(ハッシュのキーを配列として返す)
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.keys #=> [:japan, :us, :india]
+
+#values(ハッシュの値を配列として返す)
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.values #=> ["yen", "dollar", "rupee"]
+
