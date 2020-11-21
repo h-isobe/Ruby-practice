634cce24857c1d669ce34688de545296c1a45647
磯部浩樹 <h.sep23@gmail.com>
2020-11-21 14:01:17 +0900

    [Update]chapter5-2

--- cherry-book/5章/chapter5-2.rb
+++ cherry-book/5章/chapter5-2.rb
@@ -28,3 +28,38 @@ name = 'Alice'
 # "文字列: 値" の形式で書くと、キーがシンボルになる
 hash = { 'abc': 123} #=> { :abc=>123 }
 
+
+## %記法でシンボルやシンボルの配列を作成する
+
+#シンボルを作成する場合は%sを使う
+# !を区切り文字に使う
+%s!ruby is fun! #=> :"ruby is fun"
+
+# ()を区切り文字に使う
+%s(ruby is fun) #=> :"ruby is fun"
+
+#シンボルの配列を作成する場合は、%iを使うことができる。空白文字が要素の区切りになる
+%i(apple orange melon) #=> [:apple, :orange, :melon]
+
+
+##シンボルと文字列の関係
+
+#別物なので互換性はない
+string = 'apple'
+symbol = :apple
+
+string == symbol #=> false
+string + symbol  #=> TypeError (no implicit conversion of Symbol into String)
+
+#ただし、to_symメソッド(エイリアスメソッドはintern)を使うと、文字列をシンボルに変換できる
+string = 'apple'
+symbol = ':apple'
+string.to_sym           #=> :apple
+string.to_sym == symbol #=> true
+
+#反対に、to_sメソッド(エイリアスメソッドはid2name)を使うと、シンボルを文字列に変換できる
+string = 'apple'
+symbol = ':apple'
+symbol.to_s           #=> "apple"
+string == symbol.to_s #=> true
+string + symbol.to_s  #=> "appleapple"
