bca11d8cdd3f1d6d99230e3bd07887c625f7ef0f
磯部浩樹 <h.sep23@gmail.com>
2020-11-27 09:24:39 +0900

    [Update]test_deep_freeze

--- cherry-book/test/deep_freezable_test.rb
+++ cherry-book/test/deep_freezable_test.rb
@@ -1,8 +1,23 @@
 require 'minitest/autorun'
-require './8章/deep_freezable'
+require './8章/bank'
+require './8章/team'
 
 class DeepFreezableTest < Minitest::Test
-  def test_deep_freeze
-    assert DeepFreezable
+  def test_deep_freeze_to_array
+    # 配列の値は正しいか？
+    assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
+    # 配列自身がfreezeされているか？
+    assert Team::COUNTRIES.frozen?
+    # 配列の要素がすべてfreezeされているか？
+    assert Team::COUNTRIES.all? { |country| country.frozen? }
+  end
+
+  def test_deep_freeze_to_hash
+    # ハッシュの値は正しいか？
+    assert_equal( { 'Japan' => 'yen', 'US' => 'doller', 'India' => 'rupee' }, Bank::CURRENCIES )
+    # ハッシュ自身がfreezeされているか？
+    assert Bank::CURRENCIES.frozen?
+    # ハッシュの要素(キーと値)がすべてfreezeされているか？
+    assert Bank::CURRENCIES.all? { |key, value| key.frozen? && value.frozen? }
   end
 end
