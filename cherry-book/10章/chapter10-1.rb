e7b24fc61c5cfc635cc2b10fc5cb336440767034
磯部浩樹 <h.sep23@gmail.com>
2020-11-28 11:34:04 +0900

    [Update]chapter10-1

--- cherry-book/10章/chapter10-1.rb
+++ cherry-book/10章/chapter10-1.rb
@@ -93,3 +93,71 @@ end
 #=> おはよう
 #   こんにちはこんにちはnil
 #   こんばんは
+
+
+## ブロックを引数として明示的に受け取る
+## ブロックを引数として受け取る場合は、引数の前に&をつける。実行する場合はcallメソッドを使う
+
+# ブロックをメソッドの引数として受け取る
+def greeting(&block) # 引数名は自由
+  puts 'おはよう'
+  # callメソッドを使ってブロックを実行する
+  text = block.call('こんにちは')
+  puts text 
+  puts 'こんばんは'
+end
+
+greeting do |text|
+  puts text * 2
+end
+#=> おはよう
+#   こんにちはこんにちは
+#   こんばんは
+
+
+# ブロックを引数で受け取る場合でも、yieldやblock_given?メソッドは使用可能
+def greeting(&block)
+  puts 'おはよう'
+  if block_given?
+    text = yield 'こんにちは'
+    puts text
+  end
+  puts 'こんばんは'
+end
+# 上記のままではブロックを引数で受け取るメリットがない
+# メリットは、greetingメソッドのブロック引数を他のメソッドに渡せること
+
+# 日本語版のgreetingメソッド。ここではブロックを受け取るだけ。
+def greeting_ja(&block)
+  texts = ['おはよう', 'こんにちは', 'こんばんは']
+  greeting_common(texts, &block)
+end
+
+# 英語版のgreetingメソッド。ここではブロックを受け取るだけ。
+def greeting_en(&block)
+  texts = ['good morning', 'hello', 'good evening']
+  greeting_common(texts, &block)
+end
+
+# 出力処理用の共通メソッド
+def greeting_common(texts, &block)
+  puts texts[0]
+  puts block.call(texts[1])
+  puts texts[2]
+end
+
+# 日本語版のgreetingメソッドを呼び出す
+greeting_ja do |text|
+  text * 2
+end
+#=> おはよう
+#   こんにちはこんにちは
+#   こんばんは
+
+# 英語版のgreetingメソッドを呼び出す
+greeting_en do |text|
+  text.upcase
+end
+#=> good morning
+#   HELLO
+#   good evening
