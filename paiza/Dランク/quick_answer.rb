2e152276c40146d15c09f5ced249e173ea0ab2f3
磯部浩樹 <h.sep23@gmail.com>
2020-11-29 12:14:11 +0900

    [Update]hayatoki

--- paiza/Dランク/quick_answer.rb
+++ paiza/Dランク/quick_answer.rb
@@ -15,7 +15,7 @@ a = 180 * (n - 2)
 puts a / n
 
 
-# 【早解3】うなりの周波数 
+#【早解3】うなりの周波数 
 f1,f2 = gets.split.map(&:to_i)
 if f1 <= f2
     puts f2 - f1
@@ -27,11 +27,18 @@ n = gets.split.map(&:to_i)
 puts (n[0] - n[1]).abc
 
 
-# 【早解4】たぶん...
+#【早解4】たぶん...
 puts gets.gsub(/, maybe./, '!!') 
 
 
-# 【早解6】式の計算
+#【早解5】アンダーライン
+a = []
+a << gets
+a << "^" * a[0].length
+puts a
+
+
+#【早解6】式の計算
 a = gets.split(" ").map(&:to_i).inject(:+)
 puts a
 
@@ -43,6 +50,22 @@ pencil_type = ["6B", "5B", "4B", "3B", "2B", "B", "HB", "F", "H", "2H", "3H", "4
 puts pencil_type[b]
 
 
+#【早解8】靴のサイズ
+size = gets.to_f
+a = size - 18.0, size - 18.5
+puts a.join(" ")
 
+
+#【早解9】30人31脚
+human_legs = gets.to_i + 1
+puts human_legs
+
+
+#【早解10】回数分同じ処理を繰り返す
+input_line = gets.to_i
+a = ["#", "."].map { |n| n * 10 }
+input_line.times do
+    puts a
+end
 
 
