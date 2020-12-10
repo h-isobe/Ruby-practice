7cb59a9478bbbb4ecad623cd73b9f5b14cb5f049
磯部浩樹 <h.sep23@gmail.com>
2020-12-10 16:03:32 +0900

    [Update]string_problem

--- practice-exercise/string.rb
+++ practice-exercise/string.rb
@@ -18,3 +18,35 @@ p 'HELLO'.downcase
 str = "こんにちは,おはよう,ruby"
 str.split(",")
 #=> ["こんにちは", "おはよう", "ruby"]
+
+
+## 5:rubyで"こおんはによちうわ"を"おはよう"と出力する
+str = "こおんはによちうわ"
+str.each_char.with_index do |c, index|
+  if index.odd?
+    print c
+  end
+end
+
+
+## 6:rubyで以下を出力した場合は、選択肢１と２どちらで表示されるか？
+str = "こんにちは, こんばんわ"
+  str.delete!("こ")
+p str 
+#=> "んにちは, んばんわ"
+# 破壊的メソッド化しているため
+
+
+## 7:rubyで文字列を挿入する
+str = "Ruby,Python,PHP"
+str.insert(4, ",Java")
+p str
+#=> "Ruby,Java,Python,PHP"
+
+
+## 8:rubyで文字列を挿入する:応用編
+str = "ありがとう,ごめんなさい,すいません"
+str.insert(5, ",おはよう")
+str.insert(-1, ",こんばんは")
+puts str
+#=> "ありがとう,おはよう,ごめんなさい,すいません,こんばんは"
