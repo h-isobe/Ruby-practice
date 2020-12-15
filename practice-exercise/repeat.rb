d2fa151db7ff5f62f633f94b60b7db86366b08e5
磯部浩樹 <h.sep23@gmail.com>
2020-12-15 12:29:36 +0900

    [Update]repeat_problem

--- practice-exercise/repeat.rb
+++ practice-exercise/repeat.rb
@@ -25,3 +25,23 @@ end
 5.times do
   puts "ruby"
 end
+
+
+## 5: 繰り返し処理.timesメソッドの挙動２
+5.times do |i|
+  puts "#{i + 1}" + ":" + "ruby"
+end
+
+
+## 6: 繰り返し処理　後置　unless
+[5, 6].each do |a|
+  next unless a == 6 # もしaが6でなかったらnextでスキップして次の処理
+  puts a
+end
+#=> 6
+
+
+## 7: 繰り返し処理.all?
+num=[1,3,5]
+p num.all?(&:odd?)
+#=> true
