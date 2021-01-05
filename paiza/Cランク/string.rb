e4e32979f95deb4f236884ede77b44c9b74a0c67
磯部浩樹 <h.sep23@gmail.com>
2021-01-05 10:52:14 +0900

    [Update]string_menu

--- paiza/Cランク/string.rb
+++ paiza/Cランク/string.rb
@@ -12,3 +12,12 @@ puts string = gets.chomp.gsub('/', '・').gsub(':', '・').split('・')
 
 ## 日時データの変換その２
 puts string = gets.chomp.gsub('/', ' ').gsub(':', ' ').split(' ')
+
+
+## 数値判定
+s = gets.chomp
+if s.match(/\A[0-9]+\z/)
+  puts "YES"
+else
+  puts "NO"
+end
