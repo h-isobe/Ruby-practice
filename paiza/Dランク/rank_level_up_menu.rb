a943a48ae8f3e2230031916b69a53f335538d28f
磯部浩樹 <h.sep23@gmail.com>
2021-01-17 14:15:50 +0900

    [Update]rank_up_menu

--- paiza/Dランク/rank_level_up_menu.rb
+++ paiza/Dランク/rank_level_up_menu.rb
@@ -1,12 +1,12 @@
 ### Cランクレベルアップメニュー
 
 
-## 単純な入出力
+## STEP: 1 単純な入出力
 string = gets
 puts string
 
 
-## 複数行にわたる出力
+## STEP: 2 複数行にわたる出力
 i = gets.to_i
 num = 'paiza'
 i.times do |num|
@@ -14,14 +14,22 @@ i.times do |num|
 end
 
 
-## 複数行にわたる入力
+## STEP: 3 複数行にわたる入力
 count = gets.to_i
 count.times do
     puts i = gets.to_i
 end
 
 
-## 半角スペース区切りでの出力
+## STEP: 5 半角スペース区切りでの出力
 i = gets.to_i
 array = ["paiza"] * i
 puts array.join(' ')
+
+
+## STEP: 6 改行区切りでの出力
+count = gets.chomp.to_i
+string = gets.split
+string.each do |str|
+    puts str
+end
